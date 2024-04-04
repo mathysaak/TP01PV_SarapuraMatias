0476440f06bd85b981e7f0c641d8f7486806097e
mathy <matiwordgit@gmail.com>
2024-04-04 15:17:08 -0300

    Promedio ejercicio01

---
+++ src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 8, n2 = 5, n3 = 1, n4 = 20, n5 = 9;
+		double promedio = (double)(n1+n2+n3+n4+n5)/5;
+		
+		System.out.println(promedio);
+		
+	}
+
+}
